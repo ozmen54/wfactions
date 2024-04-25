b506a2e8ea65f43a052da9bd9b872d8e667094aa
Ahmet Özmen <ozmen@sakarya.edu.tr>
2024-04-25 09:56:32 +0300

    Initial commit.

---
+++ src/main/java/org/sau/wfactions/WfactionsApplication.java
@@ -0,0 +1,13 @@
+package org.sau.wfactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WfactionsApplication.class, args);
+    }
+
+}
